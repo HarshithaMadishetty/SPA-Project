88d20a4f742ba12e632ecf52819a07a1da921fb1
harshitham5 <73735816+harshitham5@users.noreply.github.com>
2024-09-22 21:58:20 +0530

    Modified

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -4,14 +4,22 @@ import axios from 'axios';
 const Login = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [role, setRole] = useState('user'); // Default role is 'user'
 
     const handleLogin = async (e) => {
         e.preventDefault();
         try {
             const response = await axios.post('/api/users/login', { username, password });
-            alert(response.data.message);
+            console.log(response.data);
+            if(response.data.success){
+                alert("Login Successful");
+            }else{
+                //alert(response.data.message);
+                alert("Login did'nt occur");
+            }
+            
         } catch (error) {
-            alert('Login failed');
+            alert('Login failed'+(error.response ? error.response.data.message : "Server error"));
         }
     };
 
@@ -21,6 +29,10 @@ const Login = () => {
             <form onSubmit={handleLogin}>
                 <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} required />
                 <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+                <select class="role" value={role} onChange={(e) => setRole(e.target.value)} required>
+                    <option value="user">User</option>
+                    <option value="admin">Admin</option>
+                </select>
                 <button type="submit">Login</button>
             </form>
         </div>
--- frontend/src/Register.js
+++ frontend/src/Register.js
@@ -9,7 +9,13 @@ const Register = () => {
         e.preventDefault();
         try {
             const response = await axios.post('/api/users/register', { username, password });
-            alert(response.data.message);
+            
+            if(response.data.success){
+                alert("Registeration Successful");
+            }else{
+                alert(response.data.message || "Registeration failed");
+            }
+            
         } catch (error) {
             alert('Registration failed');
         }
@@ -21,6 +27,7 @@ const Register = () => {
             <form onSubmit={handleRegister}>
                 <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} required />
                 <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+                
                 <button type="submit">Register</button>
             </form>
         </div>
